0495c85aeff3b7c446199a1048da7f6a3bc030d8
Spin Rodriguez <favorspin@users.noreply.github.com>
2020-02-17 22:11:54 +0000

    updated help

--- src/commands/help.js
+++ src/commands/help.js
@@ -14,7 +14,8 @@ const handler = (payload, res) => {
         title: 'Parkingbot can help you move the car that\'s blocking you in!',
         color: '#2FA44F',
         text: '`/parking add <LICENSE>` will register your license plate so that moves can be requested. \
-               \n`/parking move <LICENSE>` requests the owner of the car to move so you can get out.',
+               \n`/parking move <LICENSE>` requests the owner of the car to move so you can get out. \
+               \n`/parking list` will list all Licnese plates attached to your user.',
         mrkdwn_in: ['text']
     }]
 
