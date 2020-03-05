eeafe6a1bbe6b85b9eb166a9aec5625705012ef3
Spin Rodriguez <favorspin@users.noreply.github.com>
2020-03-05 19:05:56 +0000

    try again

--- src/commands/remove.js
+++ src/commands/remove.js
@@ -19,8 +19,8 @@ const handler = async (payload, res) => {
     } else {
         let my_car = await query.getUsernameByPlate(plate)
 
-        console.log(requester_id)
-        console.log(my_car)
+        console.log('requester_id: ' + requester_id)
+        console.log('my_car: ' + my_car)
 
         if (my_car != requester_id && !is_admin) {
             response_text = 'You are not allowed to remove cars that aren\'t yours.'
